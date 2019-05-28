1a368906cf159005bcb26ac3533d3eb0e7031dba
Chris Morgan <bluemorgan2323@gmail.com>
2019-05-28 13:59:32 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Author: Chris Morgan
+    Date: May 28th, 2019
+    Exercise: Randall's Car Lot C# Challenge
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace randalls_lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
